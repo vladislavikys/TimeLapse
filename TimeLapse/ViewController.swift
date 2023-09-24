8f9a8e7dc800d66c82e25bccd98cf9e3fad29ffa
vladislavikys <zarya.vlad@yandex.ru>
2023-09-24 10:07:58 +0300

    Initial Commit

---
+++ TimeLapse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeLapse
+//
+//  Created by Влад on 24.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
